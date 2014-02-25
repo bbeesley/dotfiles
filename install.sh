dcc22662d403af27b5a46c02e6cb31ef1cfa3530
Bill Beesley <bbeesley@me.com>
2014-02-25 14:36:38 +0000

    Adding directory setup to install script

--- install.sh
+++ install.sh
@@ -27,6 +27,37 @@ echo "You don't seem to have a $dotname in your home folder, linking the one fro
 ln -s ~/dotfiles/$i ~/$dotname
 fi
 done
+for i in $(ls ~/dotfiles | grep '^_'); do 
+dotname=$(echo $i | sed 's|^_|.|')
+if [[ ~/$dotname ]]
+then
+echo "You already have a $dotname in your home folder, I'll move it to the dotfiles directory and symlink it back"
+mv ~/$dotname ~/dotfiles/$i && ln -s ~/dotfiles/$i ~/$dotname
+else 
+echo "You don't seem to have a $dotname in your home folder, linking the one from the dotfiles directory"
+ln -s ~/dotfiles/$i ~/$dotname
+fi
+done
+
+for i in $(find ~/dotfiles/ -maxdepth 2 -mindepth 2 -type d | grep -v '\.git'); do
+dotdir=$(dirname $i | grep -o '[^/]*$')
+sub=$(basename $i)
+if [[ ~/.$dotdir ]]
+then
+echo "OK, .$dotdir exists in your home dir"
+if [[ ~/.$dotdir/$sub ]]
+then
+echo "OK, you already have a local version of.$dotdir/$sub, moving it to dotfiles repo and symlinking"
+mv ~/.$dotdir/$sub ~/dotfiles/$dotdir/$sub && ln -s ~/dotfiles/$dotdir/$sub ~/.$dotdir/$sub
+else
+echo "No $sub in your .$dotdir, symlinking the version from the dotfiles repo"
+ln -s ~/dotfiles/$dotdir/$sub ~/.$dotdir/$sub
+fi
+else
+echo "You dont have .$dotdir in your home, making it and symlinking $sub from the dotfiles repo"
+mkdir ~/.$dotdir && ln -s ~/dotfiles/$dotdir/$sub ~/.$dotdir/$sub
+fi
+done
 }
 
 ## Do the branch test
