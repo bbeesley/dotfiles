c904c0bbbb9479cb542f2d0f44baa953b64b163b
Bill Beesley <bbeesley@me.com>
2019-09-05 22:52:49 +0100

    chore: remove brackets config

--- config/Brackets/Brackets/extensions/user/mackenza.r-syntax/README.md
+++
@@ -1,2 +0,0 @@
-# brackets-R-syntax
-Does nothing more than expose the R programming language mode from CodeMirror to Brackets.
--- config/Brackets/extensions/user/brackets-nodejs/test/strict-fail.js
+++
@@ -1,4 +0,0 @@
-// This file should error when you set flags fir v8 to --use-strict:
-// ReferenceError: foo is not defined
-
-foo = "bar";
